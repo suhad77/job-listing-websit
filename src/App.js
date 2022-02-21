56a036df196264ef79c871fb8ab5c5a2253531ef
Suhad Mia <suhadmia12@gmail.com>
2022-02-21 23:37:16 +0600

    some file add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import JobListing from "./JobListing";
+import React, { useState, useEffect} from 'react';
+import Data from './assets/data.json';
+import JobBoardCompornet from "./components/JobBoardComponent";
+
+console.log(Data);
+
 function App() {
+  const [jobs, setjobs] = useState([]);
+
+
+  useEffect(() => setjobs(Data), [] );
   return (
-    <JobListing />
-  );
+    <div className="App">
+      <h1> Hello Suhad! </h1>
+      {
+        jobs.length === 0 ? (
+          <p>jobs are fetching...</p>
+        ) : (
+          jobs.map((job) => <JobBoardCompornet job={job} key={job.id} />)
+        )
+      }
+    
+    </div>
+  ); 
 }
 
 export default App;
---
+++ src/components/JobBoardComponent.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const JobBoardCompornet = () => ( 
+        <div>Hi I am a job board! </div>
+
+    )
+
+export default JobBoardCompornet;
+
