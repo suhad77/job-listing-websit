63568d1be42bcba50c62d150e779081988ab6b80
Suhad Mia <suhadmia12@gmail.com>
2022-02-26 23:49:02 +0600

    some file add

--- src/App.js
+++ src/App.js
@@ -7,8 +7,29 @@ console.log(Data);
 function App() {
   const [jobs, setjobs] = useState([]);
 
+  const [filters, setFilters] = useState([]);
 
   useEffect(() => setjobs(Data), [] );
+
+  const filterFunc = ({role, level,tools,languages}) => {
+    if(filters.length ===0){
+      return true;
+    }
+
+    const tags = [ role, level]
+
+    if(languages) {
+        tags.push(...languages);
+    }
+    if(tools) {
+        tags.push(...tools);
+    }
+
+    return tags.some(tag => filters.includes(tag));;
+  }
+
+  const filtereJobs = jobs.filter(filterFunc);
+
   return (
     <div className="App">
       <header bg-teal-500 mb-12>
